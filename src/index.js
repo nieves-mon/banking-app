4169e4bcd87d6b56d233b8fe2a10a82d47a8004a
Nieves Montoya <cunanan.nm@gmail.com>
2022-07-30 07:45:58 +0800

    Move main.css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from "./components/App";
-import "./styles/main.css";
+import "./main.css";
 
 const root = createRoot(document.getElementById("root"));
 
