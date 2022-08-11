8eeffdb678fd7b02871ded5855101061a3be7b9f
Nieves Montoya <cunanan.nm@gmail.com>
2022-08-11 16:44:04 +0800

    Create UsersContext

---
+++ src/contexts/UsersContext.js
@@ -0,0 +1,31 @@
+import React, { createContext, useState } from "react";
+
+export const UsersContext = createContext();
+
+export const UsersProvider  = ({ children }) => {
+    const [users, setUsers] = useState(
+        JSON.parse(localStorage.getItem("users")) === null ? [] : JSON.parse(localStorage.getItem("users"))
+    );
+
+    const [currUser, setCurrUser] = useState(
+        JSON.parse(localStorage.getItem("currentUser")) === null ? {} : JSON.parse(localStorage.getItem("currentUser"))
+    );
+
+    const updateUsers = (newUsers) => {
+        localStorage.setItem("users", JSON.stringify(newUsers));
+        setUsers(newUsers);
+        return;
+    }
+
+    const changeCurrUser = (user) => {
+        localStorage.setItem("currentUser", JSON.stringify(user));
+        setCurrUser(user);
+        return;
+    }
+
+    return (
+        <UsersContext.Provider value={[users, updateUsers, currUser, changeCurrUser]}>
+            {children}
+        </UsersContext.Provider>
+    );
+}
