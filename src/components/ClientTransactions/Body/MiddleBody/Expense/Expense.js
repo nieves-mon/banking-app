c2be9f937a4397831fea365ae6f8c6c60737eae3
Nieves Montoya <cunanan.nm@gmail.com>
2022-08-12 17:45:04 +0800

    Add Expense chart

--- src/components/ClientTransactions/Body/MiddleBody/Expense/Expense.js
+++ src/components/ClientTransactions/Body/MiddleBody/Expense/Expense.js
@@ -1,6 +1,7 @@
 import React, {useContext, useEffect, useState} from "react";
 import { PageContext } from "../../../../../contexts/PageContext";
 import { UsersContext } from "../../../../../contexts/UsersContext";
+import CustomPieChart from "../CustomPieChart/CustomPieChart";
 import "./Expense.css";
 
 const Expense = ({cost, setCost, updateBalance}) => {
@@ -26,51 +27,78 @@ const Expense = ({cost, setCost, updateBalance}) => {
         addExpense();
     }
 
+    const totalCost = (catName) => {
+        let total = 0;
+
+        for(let i = 0; i < currUser.expenses.length; i++) {
+            if(currUser.expenses[i].category === catName) {
+                total += parseFloat(currUser.expenses[i].cost);
+            }
+        }
+
+        return total;
+    }
+
+    const categoryCount = [
+        {"name": "Rent", "value": totalCost("Rent")},
+        {"name": "Food", "value": totalCost("Food")},
+        {"name": "Transportation", "value": totalCost("Transportation")},
+        {"name": "Mortgage", "value": totalCost("Mortgage")},
+        {"name": "Utilities", "value": totalCost("Utilities")},
+        {"name": "Entertainment", "value": totalCost("Entertainment")}
+    ];
+
     useEffect(() => {
         setPage("expense");
     }, [setPage]);
 
     return (
         <div className="transactionContainer add-expense-div">
-            <form className="transaction" onSubmit={e => handleConfirm(e)}>
-                <h4 className="form-header">Add Expense</h4>
-                <div className="row-container">
-                    <div className="inputContainer">
-                        <label htmlFor="cost">Cost</label>
-                            <input
-                                required
-                                id="cost"
-                                className="inputValue"
-                                name="expense-cost"
-                                type="number"
-                                min="0.0"
-                                step="any"
-                                value={cost}
-                                onChange={e => setCost(e.target.value)}
-                            />
+            <div className="expense-top">
+                <form className="transaction" onSubmit={e => handleConfirm(e)}>
+                    <h4 className="form-header">Add Expense</h4>
+                    <div className="row-container">
+                        <div className="inputContainer">
+                            <label htmlFor="cost">Cost</label>
+                                <input
+                                    required
+                                    id="cost"
+                                    className="inputValue"
+                                    name="expense-cost"
+                                    type="number"
+                                    min="0.0"
+                                    step="any"
+                                    value={cost}
+                                    onChange={e => setCost(e.target.value)}
+                                />
+                        </div>
+                        <div className="inputContainer">
+                            <label htmlFor="type">Type</label>
+                            <select>
+                                {currUser.categories.map(cat => {
+                                    return <option key={cat} value={cat} onClick={() => setCategory(cat)}>{cat}</option>
+                                })}
+                            </select>
+                        </div>
                     </div>
                     <div className="inputContainer">
-                        <label htmlFor="type">Type</label>
-                        <select>
-                            {currUser.categories.map(cat => {
-                                return <option key={cat} value={cat} onClick={() => setCategory(cat)}>{cat}</option>
-                            })}
-                        </select>
+                        <label htmlFor="desc">Description</label>
+                        <textarea
+                            id="desc"
+                            name="expense-desc"
+                            value={desc}
+                            onChange={e => setDesc(e.target.value)}
+                        />
                     </div>
+                    <div className="submitButton">
+                        <button className="button expense-btn" onClick={e => handleConfirm(e)}>Add Expense</button>
+                    </div>
+                </form>
+
+                <div className="expense-chart">
+                    <CustomPieChart data={categoryCount}/>
                 </div>
-                <div className="inputContainer">
-                    <label htmlFor="desc">Description</label>
-                    <textarea
-                        id="desc"
-                        name="expense-desc"
-                        value={desc}
-                        onChange={e => setDesc(e.target.value)}
-                    />
-                </div>
-                <div className="submitButton">
-                    <button className="button expense-btn" onClick={e => handleConfirm(e)}>Add Expense</button>
-                </div>
-            </form>
+            </div>
             <div className="expense-list-div">
                 <table className="expense-list">
                     <thead>
